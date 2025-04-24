6b15cc8ce024d3ee626f37179aef0ac5d0020373
pieakshat <akshat.05p@gmail.com>
2025-04-24 23:47:34 +0530

    arcjet integrated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDb from "./Database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 
 const app = express()
@@ -25,6 +26,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/subscription', subscriptionRouter);
 app.use(errorMiddleware);
+app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcom to the subcription tracker API');
